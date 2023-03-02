b2612140f8ceccbb063dbd0f09d8692b245c305d
IsakNyberg21F <isak.nyberg@elev.ga.ntig.se>
2023-03-02 21:31:04 +0100

    fixed animations

--- code.js
+++ code.js
@@ -1,9 +1,20 @@
+const sideBar = document.querySelector('.content');
+const overlay = document.querySelector('.overlay');
+
+window.addEventListener('click', function(event) {
+    console.log("hejejejejeje");
+    if (event.target !== sideBar && document.getElementById("popup-1").classList.contains('active')) {
+        document.getElementById("popup-1").classList.remove("active");
+        console.log("hejejejejeje");
+    }
+});
+
 function toggleMenuSidebar(){
     document.getElementById("popup-1").classList.toggle("active");
 }
 
 document.addEventListener('keydown', function(event){
 	if(event.key === "Escape" && document.getElementById("popup-1").classList.contains('active')){
-        document.getElementById("popup-1").classList.toggle("active")
+        document.getElementById("popup-1").classList.toggle("active");
 	}
 });
